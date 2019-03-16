e31f583bf01085f63e0e3784b11b8f76f30744c5
tamasaka <kuroshita96410@gmail.com>
2019-03-16 19:05:14 +0900

    python practice

---
+++ scraping.py
@@ -0,0 +1,23 @@
+import urllib3
+from bs4 import BeautifulSoup
+
+url="http://www.nikkei.com/"
+
+#html=urllib3.urlopen(url)
+http=urllib3.PoolManager()
+#プールを作成
+response=http.request('GET',url)
+#requestでｈｔｍｌを取得
+
+
+#soup=BeautifulSoup(html,"html.parser")
+soup=BeautifulSoup(response.data,'html.parser')
+
+
+title_wrap=soup.title
+
+title=title_wrap.string
+
+print (title_wrap)
+
+print (title)
